5764c6ef3b6f888b1138311cf892b91af9dc7754
ELDevelopero <49122792+ELDevelopero@users.noreply.github.com>
2020-09-03 16:50:18 +0200

    6thLabNotFinishedYet

---
+++ WebApi/EntityFW/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFW.Models
+{
+    public partial class Products
+    {
+        public Products()
+        {
+            Ratings = new HashSet<Ratings>();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public virtual ICollection<Ratings> Ratings { get; set; }
+    }
+}
---
+++ WebApi/EntityFW/Models/Ratings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFW.Models
+{
+    public partial class Ratings
+    {
+        public string Id { get; set; }
+        public int Rate { get; set; }
+        public string ProductId { get; set; }
+        public string Comment { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public virtual Products Product { get; set; }
+    }
+}
