737a54e922629d7f0401fbab90592ec0fa3006c3
ELDevelopero <49122792+ELDevelopero@users.noreply.github.com>
2020-09-07 16:08:24 +0200

    7thFinished

---
+++ WebApi/APIwithIdentity/Entities/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIwithIdentity.Entities
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
